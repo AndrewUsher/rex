cc04353e4d62ecbbae231b95baeaf74e9903e497
Andrew Darts <mandrewdarts@gmail.com>
2017-10-24 12:59:46 -0500

    init, somewhat works

---
+++ index.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+const path = require('path')
+const puppeteer = require('puppeteer')
+const superstatic = require('superstatic').server
+const shell = require('shelljs')
+const caporal = require('caporal')
+const fs = require('fs')
+
+let routes = []
+
+caporal
+  .version('0.0.1')
+  .description('Prerender your single page app')
+  .argument('[directory]', 'Directory your site lives in')
+  .option('--routes', 'List of routes to prerender', caporal.LIST)
+  .action(async function(args, options, logger) {
+    serve(args.directory)
+    if(options.routes) {
+      const staticFiles = options.routes.map(async (route, index) => {
+	const result = await go(options, route)
+	return await saveFile(args.directory, route, result)
+      })
+
+      await waitForStaticFiles(staticFiles)
+      process.exit()
+
+    } else {
+      const result = await go(options)
+      await saveFile(args.directory, '/', result)
+      process.exit()
+    }
+  })
+
+caporal.parse(process.argv)
+
+function serve(directory) {
+  const app = superstatic({
+    port: 3000,
+    config: {
+      public: directory || 'dist',
+      rewrites: [ 
+	{ source: '**', destination: '/index.html' }
+      ]
+    }
+  })
+
+  app.listen()
+}
+
+
+async function go(opts, route = '') {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('http://localhost:3000'+route)
+  
+  const contents = await page.evaluate(() => document.documentElement.outerHTML)
+
+  await browser.close()
+  return contents
+}
+
+
+async function saveFile(directory, route = '', contents) {
+  return new Promise((res, rej) => {
+  
+    if(route != '/') {
+      shell.mkdir('-p', path.join(process.cwd() + '/' + directory + route));
+    }
+
+    fs.writeFile(path.join(process.cwd() + '/' + directory + route, 'index.html'), contents, 'utf-8', (err) => {
+      if(err) rej(err)
+      res()
+    })
+
+  })
+}
+
+
+async function waitForStaticFiles(staticFiles) {
+  return Promise.all(staticFiles)
+    // .then(process.exit)
+}
